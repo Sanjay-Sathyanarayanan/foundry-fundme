67df281ed522edb781decfc6c94ec90c2f57880f
Sanjay-Sathyanarayanan <sanjay.sathyanarayanan17@gmail.com>
2025-02-24 11:55:39 +0000

    format issue fixed

--- src/PriceConverter.sol
+++ src/PriceConverter.sol
@@ -7,10 +7,9 @@ import {AggregatorV3Interface} from "@chainlink/contracts/src/v0.8/interfaces/Ag
 // Why not an interface?
 library PriceConverter {
     // We could make this public, but then we'd have to deploy it
-    function getPrice(AggregatorV3Interface priceFeed) internal view returns (uint256) {ī
+    function getPrice(AggregatorV3Interface priceFeed) internal view returns (uint256) {
         // Sepolia ETH / USD Address
         // https://docs.chain.link/data-feeds/price-feeds/addresses
-
         (, int256 answer,,,) = priceFeed.latestRoundData();
         // ETH/USD rate in 18 digit
         return uint256(answer * 10000000000);
